e6840a524e2eab0233422a5658b485ef8ecb5212
J.D. Warren <johndavid400@gmail.com>
2014-05-06 20:59:10 -0500

    Added clients

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -69,7 +69,7 @@ class ProjectsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
       #params[:project]
-      params.require(:project).permit(:name, :attachment)
+      params.require(:project).permit(:name, :attachment, :client)
     end
 
     def authorize
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140425044847) do
+ActiveRecord::Schema.define(version: 20140507015421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20140425044847) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "client"
   end
 
   create_table "users", force: true do |t|
